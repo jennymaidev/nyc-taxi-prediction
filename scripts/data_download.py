4510feeb0812ad140b2a556afde000c96af52785
Jenny Mai <nhumai2@illinois.edu>
2024-08-06 17:09:40 +1000

    update initial files

---
+++ scripts/data_download.py
@@ -0,0 +1,23 @@
+from urllib.request import urlretrieve
+import os
+
+output_relative_dir = '../data'
+
+if not os.path.exists(output_relative_dir):
+    os.makedirs(output_relative_dir)
+
+YEARS = ['2022', '2023']
+MONTHS = { '2022': range(11, 13), '2023': range(1, 5) }
+TYPES = ['yellow', 'green', 'fhv', 'fhvhv']
+URL_TEMPLATE = "https://d37ci6vzurychx.cloudfront.net/trip-data/"
+
+for type in TYPES:
+    for year in YEARS:
+        month_range = MONTHS[year]
+        for month in month_range:
+            month = str(month).zfill(2)
+            print(f"Begin month {type} for {month} of {year}:")
+            url = f'{URL_TEMPLATE}{type}_tripdata_{year}-{month}.parquet'
+            output_dir = f"{output_relative_dir}/{type}_{year}_{month}.parquet"
+            urlretrieve(url, output_dir)
+            print(f"Completed month {type} for {month} of {year}.")
